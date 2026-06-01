4af689943e10c5979a9a3b7e95802ad47333edf9
Rivoangel <161862167+Rivoangel@users.noreply.github.com>
2026-06-01 17:39:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,21 @@ document.addEventListener("DOMContentLoaded", ()=>{
 console.log("ReyaAga Website Loaded");
 
 });
+function openPopup(id){
+    document.getElementById(id).classList.add("active");
+}
+
+function closePopup(id){
+    document.getElementById(id).classList.remove("active");
+}
+
+// Close when clicking outside popup
+document.addEventListener("click", function(e){
+    const overlays = document.querySelectorAll(".overlay");
+
+    overlays.forEach(overlay=>{
+        if(e.target === overlay){
+            overlay.classList.remove("active");
+        }
+    });
+});
